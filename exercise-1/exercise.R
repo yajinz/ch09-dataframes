cf73dfd368dafa5643624bb688c64f21d9d7a87d
Ott Toomet <otoomet@uw.edu>
2017-10-11 09:22:42 -0700

    exercise-1/exercise.R: seahaws update, no row.names

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,7 +1,9 @@
-# Exercise 1: Creating data frames
+### -------------------- Exercise 1: Creating data frames --------------------
 
-# Create a vector of the number of points the Seahawks scored the first 4 games of last season
-# Hint: (google "Seahawks scores 2016")
+## Create a vector of the number of points the Seahawks scored in the recent games
+                           # Hint: google "Seahawks scores", or check the football database:
+                           # http://www.footballdb.com/teams/nfl/seattle-seahawks/results
+                           # here 'Result' displays scores, 'L' stands for loss, 'W' for win
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
 
@@ -12,10 +14,10 @@
 # Create a new column "diff" that is the difference in points
 
 
-# Create a new column "won" which is TRUE if the Seahawks wom
+# Create a new column "won" which is TRUE if the Seahawks won
 
 
 # Create a vector of the opponents
 
 
-# Assign your dataframe rownames of their opponents
+# Add the vector of opponents into the data frame
