8e631dcc0875a8862f0ae3de0961a0ff40f12f9e
andrewk7 <andrewk7@uw.edu>
2017-10-11 10:04:57 -0700

    Updated solutions to reflect change in exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,22 +1,30 @@
-# Exercise 1: Creating data frames
+### -------------------- Exercise 1: Creating data frames --------------------
 
-# Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-points <- c(12, 3, 37, 27)
+## Create a vector of the number of points the Seahawks scored in the first 5 games
+# Hint: google "Seahawks scores", or check the football database:
+# http://www.footballdb.com/teams/nfl/seattle-seahawks/results
+# here 'Result' displays scores, 'L' stands for loss, 'W' for win
+points <- c(9, 12, 27, 46, 16)
 
-# Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-points.allowed <- c(10, 9, 18, 17)
+# Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 5 games
+points.allowed <- c(17, 9, 33, 18, 10)
+
 
 # Combine your two vectors into a dataframe
 games <- data.frame(points, points.allowed)
 
+
 # Create a new column "diff" that is the difference in points
 games$diff <- games$points - games$points.allowed
 
-# Create a new column "won" which is TRUE if the Seahawks wom
+
+# Create a new column "won" which is TRUE if the Seahawks won
 games$won <- games$diff > 0
 
-# Create a vector of the opponents
-opponents <- c('Dolphins', 'Rams', '49ers', 'Jets')
 
-# Assign your dataframe rownames of their opponents
+# Create a vector of the opponents
+opponents <- c('Green Bay', 'San Francisco', 'Tennessee', 'Indianapoli', 'LA Rams')
+
+
+# Add the vector of opponents into the data frame
 rownames(games) <- opponents
